eca93eca7410c382932fd5a02d3c8bbfb28f7313
Wouter Alewijnse <110819@spinoza.local>
2022-02-02 11:09:14 +0100

    Add project files.

---
+++ Functioneel-Ontwerp-Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Functioneel_Ontwerp_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
