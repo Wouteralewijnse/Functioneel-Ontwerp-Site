389b1a688d4fef915586ae0100ef6890308be6a7
Alex Steij <110718@st-maartenscollege.nl>
2022-04-06 11:48:52 +0200

    data connecter eerste stap

--- Functioneel-Ontwerp-Site/Controllers/HomeController.cs
+++ Functioneel-Ontwerp-Site/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data;
 
 namespace Functioneel_Ontwerp_Site.Controllers
 {
