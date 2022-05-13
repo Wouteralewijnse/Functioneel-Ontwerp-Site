30df127ed560d85e26fa2a238220d593c3c8d386
110819@st-maartenscollege.nl <110819@st-maartenscollege.nl>
2022-05-13 12:44:52 +0200

    Product.cs toegevoegd

---
+++ Functioneel-Ontwerp-Site/Database/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Functioneel_Ontwerp_Site.Database
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Naam { get; set; }
+        public string? Prijs { get; set; }
+
+        public int Beschikbaarheid { get; set; }
+        
+    }
+}
